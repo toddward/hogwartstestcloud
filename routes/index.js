ca23664394cc240de7cc89248540ae032ea29748
Wm. Todd Wardzinski <wardzinski.todd@gmail.com>
2018-10-03 16:41:18 -0400

    Thorwing an abritrary timeout to lengthen run time.

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.post('/', function(req, res, next) {
-  // Get the object passed in...read the specifics.
-  var request = req.body;
+router.post('/', function (req, res, next) {
+  setTimeout(function () {
+    // Get the object passed in...read the specifics.
+    var request = req.body;
 
-  if (req.body.bravery >= 8) {
-    request.house = 'gryffendor';
-  } else if (req.body.cunning >= 8) {
-    request.house = 'slytherin';
-  } else if (req.body.loyalty >= 8) {
-    request.house = 'hufflepuff';
-  } else if (req.body.wit >= 8) {
-    request.house = 'ravenclaw'
-  }
+    if (req.body.bravery >= 8) {
+      request.house = 'gryffendor';
+    } else if (req.body.cunning >= 8) {
+      request.house = 'slytherin';
+    } else if (req.body.loyalty >= 8) {
+      request.house = 'hufflepuff';
+    } else if (req.body.wit >= 8) {
+      request.house = 'ravenclaw'
+    }
 
-  return res.status(200).json(request);
-  next();
+    return res.status(200).json(request);
+    next();
+  }, 3000); // little timeout to increase time it takes to
 });
 
 module.exports = router;
